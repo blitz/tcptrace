2501841208a1e18d87a4bf6a63412980ca3570a3
sdo <>
1994-08-08 17:00:08 +0000

    latest

---
+++ OBSOLETE/rtt.c
@@ -0,0 +1,226 @@
+/* 
+ * rtt.c - Round Trip Timing Routines
+ * 
+ * Author:	Shawn Ostermann
+ * 		Dept. of Computer Sciences
+ * 		Purdue University
+ * Date:	Fri Sep  4 13:35:42 1992
+ *
+ * Copyright (c) 1992 Shawn Ostermann
+ */
+
+#include "tcptrace.h"
+
+
+void
+seglist_init(
+    tcb *ptcb
+    )
+{
+    ptcb->seglist_head.next = &ptcb->seglist_tail;
+    ptcb->seglist_tail.prev = &ptcb->seglist_head;
+}
+
+
+
+static seg_rec *
+newseg()
+{
+    seg_rec *pnew;
+
+    pnew = (seg_rec *) malloc(sizeof(seg_rec));
+    bzero(pnew,sizeof(seg_rec));
+    return(pnew);
+}
+
+
+static void
+freeseg(
+    seg_rec *pseg
+    )
+{
+    free(pseg);
+}
+
+
+static void
+unlinkseg(
+    seg_rec *pseg
+    )
+{
+    pseg->next->prev = pseg->prev;
+    pseg->prev->next = pseg->next;
+    freeseg(pseg);
+}
+
+
+
+
+static void
+ack_in(
+    tcb *ptcb,
+    struct timeval time,
+    struct tcphdr *ptcp,
+    struct ip *pip
+    )
+{
+    unsigned long etime;
+    unsigned ack = ntohl(ptcp->th_ack);
+    seg_rec *pseg;
+    int found = 0;
+
+    /* try to find the segment(s) acked */
+    pseg = ptcb->seglist_head.next;
+    while (pseg != &ptcb->seglist_tail) {
+/* 	printf("Ack: %u  ackedby: %u\n", ack, pseg->ackedby); */
+	if (SEQ_LESSTHAN(pseg->ackedby,ack)) {
+	    /* cumulatively ACKed, just remove it */
+
+	    ++found;
+	    ++ptcb->rtt_cumack;
+	    unlinkseg(pseg);
+	} else if (pseg->ackedby == ack) {
+	    /* specific ACK */
+	    ++found;
+
+	    /* how long did it take */
+	    etime = elapsed(pseg->time,time);
+
+	    if (pseg->retrans == 0) {
+		if ((ptcb->rtt_min == 0) || (ptcb->rtt_min > etime))
+		    ptcb->rtt_min = etime;
+
+		if (ptcb->rtt_max < etime)
+		    ptcb->rtt_max = etime;
+
+		ptcb->rtt_sum += etime;
+		ptcb->rtt_sum2 += (double)etime * (double)etime;
+		++ptcb->rtt_count;
+	    } else {
+		/* retrans, can't use it */
+		if ((ptcb->rtt_min_last == 0) || (ptcb->rtt_min_last > etime))
+		    ptcb->rtt_min_last = etime;
+
+		if (ptcb->rtt_max_last < etime)
+		    ptcb->rtt_max_last = etime;
+
+		ptcb->rtt_sum_last += etime;
+		ptcb->rtt_sum2_last += (double)etime * (double)etime;
+		++ptcb->rtt_count_last;
+
+		++ptcb->rtt_amback;  /* ambiguous ACK */
+	    }
+	    
+	    unlinkseg(pseg);
+	}
+	pseg = pseg->next;
+    }
+
+    /* did we EVER find a match */
+    if (found == 0) {
+	if (ack == ptcb->ptwin->ack)
+	    ++ptcb->rtt_redack;
+	else
+	    ++ptcb->rtt_unkack;
+    }
+}
+
+
+
+static void
+seg_out(
+    tcb *ptcb,
+    struct timeval time,
+    struct tcphdr *ptcp,
+    struct ip *pip
+    )
+{
+    unsigned long etime;
+    u_long start;
+    u_long len;
+    seg_rec *pseg;
+    
+    /* find packet data */
+    start = ntohl(ptcp->th_seq);
+
+    /* calculate data length */
+    len = ntohs(pip->ip_len) - (4 * pip->ip_hl) - (4 * ptcp->th_off);
+    if (len == 0)
+	return;
+
+    /* see if it's already there */
+    pseg = ptcb->seglist_head.next;
+    while (pseg != &ptcb->seglist_tail) {
+	/* currently only works for 'exact matches' */
+	if (pseg->seq == start)
+	    break;
+	pseg = pseg->next;
+    }
+
+    if (pseg != &ptcb->seglist_tail) {
+	/* it's already there, must be retrans */
+	++pseg->retrans;
+
+	etime = elapsed(pseg->time,time);
+	if (pseg->retrans > ptcb->retr_max)
+	    ptcb->retr_max = pseg->retrans;
+
+	if (etime > ptcb->retr_max_tm)
+	    ptcb->retr_max_tm = etime;
+	if ((ptcb->retr_min_tm == 0) || (etime < ptcb->retr_min_tm))
+	    ptcb->retr_min_tm = etime;
+
+	ptcb->retr_tm_sum += etime;
+	ptcb->retr_tm_sum2 += (double)etime*(double)etime;
+	++ptcb->retr_tm_count;
+
+	pseg->time = time;
+    } else {
+	/* not found, put at end */
+
+	/* fill in fields in new seg entry */
+	pseg = newseg();
+	pseg->seq = start;
+	pseg->ackedby = start + len;
+	pseg->time = time;
+
+	/* put at the end of the list */
+	pseg->prev = ptcb->seglist_tail.prev;
+	pseg->next = &ptcb->seglist_tail;
+	ptcb->seglist_tail.prev->next = pseg;
+	ptcb->seglist_tail.prev = pseg;
+    }
+}
+
+
+
+
+void
+calc_rtt(
+    tcb *ptcb,
+    struct timeval time,
+    struct tcphdr *ptcp,
+    struct ip *pip
+    )
+{
+    seg_out(ptcb,time,ptcp,pip);
+    ack_in(ptcb->ptwin,time,ptcp,pip);
+}
+
+
+
+void print_seglist(
+    tcb *ptcb
+    )
+{
+    seg_rec *pseg;
+
+    pseg = ptcb->seglist_head.next;
+    while (pseg != &ptcb->seglist_tail) {
+	printf("  %u %u %s\n",
+	       pseg->seq,
+	       pseg->ackedby,
+	       ts(pseg->time));
+	pseg = pseg->next;
+    }
+}
--- plotter.c
+++ plotter.c
@@ -6,28 +6,50 @@ int max_plotters;
 
 
 static FILE **fplot;
-static struct last **p2plast;
+static tcb **p2plast;
 static PLOTTER plotter_ix = NO_PLOTTER;
+
+
+/*
+ * Return a string suitable for use as a timestamp in the xplot output.
+ * (Currently rounds to the nearest 1/10 millisecond)
+ */
+static char *
+xp_timestamp(time)
+    struct timeval time;
+{
+    static char bufs[4][20];	/* several of them for multiple calls in one printf */
+    static int bufix = 0;
+    unsigned secs = time.tv_sec;
+    unsigned usecs = time.tv_usec;
+    unsigned decimal = (usecs + 50) / 100;
+    char *pbuf;
+
+    pbuf = bufs[(bufix++)%4];
+
+    sprintf(pbuf,"%u.%04u",secs,decimal);
+    return(pbuf);
+}
 
 
 
 void plot_init()
 {
-	max_plotters = 2*max_tcp_pairs;
+    max_plotters = 2*max_tcp_pairs;
 
-	fplot = (FILE **) malloc(max_plotters * sizeof(FILE *));
-	if (!fplot) {
-		perror("fplot malloc");
-		exit(-1);
-	}
+    fplot = (FILE **) malloc(max_plotters * sizeof(FILE *));
+    if (!fplot) {
+	perror("fplot malloc");
+	exit(-1);
+    }
 
-	p2plast = (struct last **) malloc(max_plotters * sizeof(struct last *));
-	if (!p2plast) {
-		perror("p2plast malloc");
-		exit(-1);
-	}
-	bzero(fplot,  max_plotters * sizeof(FILE *));
-	bzero(p2plast,max_plotters * sizeof(struct last *));
+    p2plast = (tcb **) malloc(max_plotters * sizeof(tcb *));
+    if (!p2plast) {
+	perror("p2plast malloc");
+	exit(-1);
+    }
+    bzero(fplot,  max_plotters * sizeof(FILE *));
+    bzero(p2plast,max_plotters * sizeof(struct last *));
 }
 
 
@@ -35,29 +57,29 @@ void plot_init()
 char *HostLetter(ix)
      u_int ix;
 {
-	static char name[10];
-	char ch1;
-	char ch2;
+    static char name[10];
+    char ch1;
+    char ch2;
 
-	ch1 = ix / 26;
-	ch2 = ix % 26;
+    ch1 = ix / 26;
+    ch2 = ix % 26;
 	
-	if (ix < 26) {
-		sprintf(name,"%c",'a' + ch2);
-	} else if (ix < (26*26)) {
-		sprintf(name,"%c%c", 'a' + ch1 - 1, 'a' + ch2);
-	} else {
-		fprintf(stderr,"Fatal, too many hosts to name\n");
-		exit(-1);
-	}
+    if (ix < 26) {
+	sprintf(name,"%c",'a' + ch2);
+    } else if (ix < (26*26)) {
+	sprintf(name,"%c%c", 'a' + ch1 - 1, 'a' + ch2);
+    } else {
+	fprintf(stderr,"Fatal, too many hosts to name\n");
+	exit(-1);
+    }
 
-	return(name);
+    return(name);
 }
 
 
 
 char *PlotName(plast,pl)
-     struct last *plast;
+     tcb *plast;
      PLOTTER pl;
 {
 	static char filename[10];
@@ -75,94 +97,94 @@ static void DoPlot(pl, fmt, va_alist)
      char	*fmt;
      va_dcl
 {
-	va_list	ap;
-	FILE *f = NULL;
+    va_list	ap;
+    FILE *f = NULL;
 
-	va_start(ap);
+    va_start(ap);
 
-	if (!plotem)
-	    return;
-
-	if (pl == NO_PLOTTER)
-	    return;
-
-	if (pl > plotter_ix) {
-		fprintf(stderr,"Illegal plotter: %d\n", pl);
-		exit(-1);
-	}
-
-	if ((f = fplot[pl]) == NULL)
-	    return;
-
-	vfprintf(f,fmt,ap);
-	fprintf (f,"\n");
-
-	va_end(ap);
-
+    if (!plotem)
 	return;
+
+    if (pl == NO_PLOTTER)
+	return;
+
+    if (pl > plotter_ix) {
+	fprintf(stderr,"Illegal plotter: %d\n", pl);
+	exit(-1);
+    }
+
+    if ((f = fplot[pl]) == NULL)
+	return;
+
+    vfprintf(f,fmt,ap);
+    fprintf (f,"\n");
+
+    va_end(ap);
+
+    return;
 }
 
 
 int
 new_plotter(plast,title)
-     struct last *plast;
+     tcb *plast;
      char *title;
 {
-	PLOTTER pl;
-	FILE *f;
-	char *filename;
+    PLOTTER pl;
+    FILE *f;
+    char *filename;
 
-	++plotter_ix;
-	if (plotter_ix >= max_plotters) {
-		fprintf(stderr,"No more plotters\n");
-		return(NO_PLOTTER);
-	}
+    ++plotter_ix;
+    if (plotter_ix >= max_plotters) {
+	fprintf(stderr,"No more plotters\n");
+	return(NO_PLOTTER);
+    }
 
-	pl = plotter_ix;
+    pl = plotter_ix;
 
-	filename = PlotName(plast,pl);
+    filename = PlotName(plast,pl);
 
-	if (debug)
-	    fprintf(stderr,"Plotter %d file is '%s'\n", pl, filename);
+    if (debug)
+	fprintf(stderr,"Plotter %d file is '%s'\n", pl, filename);
 
-	if ((f = fopen(filename,"w")) == NULL) {
-		perror(filename);
-		return(NO_PLOTTER);
-	}
+    if ((f = fopen(filename,"w")) == NULL) {
+	perror(filename);
+	return(NO_PLOTTER);
+    }
 
-	fprintf(f,"timeval unsigned\n");
-	fprintf(f,"title\n%s\n", title);
+    fprintf(f,"timeval unsigned\n");
+    fprintf(f,"title\n%s\n", title);
 
-	fplot[pl] = f;
-	p2plast[pl] = plast;
-	plast->plotfile = strdup(filename);
-	return(pl);
+    fplot[pl] = f;
+    p2plast[pl] = plast;
+    plast->plotfile = strdup(filename);
+    return(pl);
 }
 
 
 void
 plotter_done()
 {
-	PLOTTER pl;
-	FILE *f;
-	char *fname;
+    PLOTTER pl;
+    FILE *f;
+    char *fname;
 
-	for (pl = 0; pl < plotter_ix; ++pl) {
-		if ((f = fplot[pl]) == NULL)
-		    continue;
-		if (!ignore_non_comp || Complete(p2plast[pl]->ptp)) {
-			fprintf(f,"go\n");
-			fclose(f);
-		} else {
-			fname = p2plast[pl]->plotfile;
-			if (debug)
-			    fprintf(stderr,"Removing incomplete plot file '%s'\n",
-				    fname);
-			fclose(f);
-			if (unlink(fname) != 0)
-			    perror(fname);
-		}
+    for (pl = 0; pl < plotter_ix; ++pl) {
+	if ((f = fplot[pl]) == NULL)
+	    continue;
+	if (!ignore_non_comp || Complete(p2plast[pl]->ptp)) {
+	    fprintf(f,"go\n");
+	    fclose(f);
+	} else {
+	    fname = p2plast[pl]->plotfile;
+	    if (debug)
+		fprintf(stderr,"Removing incomplete plot file '%s'\n",
+			fname);
+	    fclose(f);
+	    if (unlink(fname) != 0)
+		perror(fname);
 	}
+    }
 }
 
 
@@ -173,85 +195,79 @@ plotter_line(pl,t1,x1,t2,x2)
      struct timeval	t1,t2;
      unsigned long	x1,x2;
 {
-	DoPlot(pl,"line %u.%06u %u %u.%06u %u",
-	     t1.tv_sec, t1.tv_usec,
-	     x1,
-	     t2.tv_sec, t2.tv_usec,
-	     x2);
+	DoPlot(pl,"line %s %u %s %u",
+	     xp_timestamp(t1), x1,
+	     xp_timestamp(t2), x2);
 }
 
 
 void
 plotter_dline(pl,t1,x1,t2,x2)
-     PLOTTER pl;
-     struct timeval	t1,t2;
-     unsigned long	x1,x2;
+    PLOTTER pl;
+    struct timeval	t1,t2;
+    unsigned long	x1,x2;
 {
-	DoPlot(pl,"dline %u.%06u %u %u.%06u %u",
-	     t1.tv_sec, t1.tv_usec,
-	     x1,
-	     t2.tv_sec, t2.tv_usec,
-	     x2);
+    DoPlot(pl,"dline %s %u %s %u", xp_timestamp(t1), x1, xp_timestamp(t2), x2);
 }
 
 
 void
 plotter_diamond(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
-	DoPlot(pl,"diamond %u.%06u %u", t.tv_sec, t.tv_usec,x);
+    DoPlot(pl,"diamond %s %u", xp_timestamp(t), x);
 }
 
 
 void
 plotter_dot(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
-	DoPlot(pl,"dot %u.%06u %u", t.tv_sec, t.tv_usec,x);
+    DoPlot(pl,"dot %s %u", xp_timestamp(t), x);
 }
 
 
 void
 plotter_plus(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
-	DoPlot(pl,"plus %u.%06u %u", t.tv_sec, t.tv_usec,x);
+    DoPlot(pl,"plus %s %u", xp_timestamp(t), x);
 }
 
 
 void
 plotter_box(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
-	DoPlot(pl,"box %u.%06u %u", t.tv_sec, t.tv_usec,x);
+    DoPlot(pl,"box %s %u", xp_timestamp(t), x);
 }
 
 
 
 void
 plotter_arrow(pl,t,x,dir)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
-     char	dir;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
+    char	dir;
 {
-	DoPlot(pl,"%carrow %u.%06u %u", dir, t.tv_sec, t.tv_usec,x);
+    DoPlot(pl,"%carrow %s %u", dir, xp_timestamp(t), x);
 }
 
 
 void
 plotter_uarrow(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_arrow(pl,t,x,'u');
 }
@@ -259,9 +275,9 @@ plotter_uarrow(pl,t,x)
 
 void
 plotter_darrow(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_arrow(pl,t,x,'d');
 }
@@ -269,9 +285,9 @@ plotter_darrow(pl,t,x)
 
 void
 plotter_rarrow(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_arrow(pl,t,x,'r');
 }
@@ -279,9 +295,9 @@ plotter_rarrow(pl,t,x)
 
 void
 plotter_larrow(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_arrow(pl,t,x,'l');
 }
@@ -289,20 +305,20 @@ plotter_larrow(pl,t,x)
 
 void
 plotter_tick(pl,t,x,dir)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
-     char	dir;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
+    char	dir;
 {
-	DoPlot(pl,"%ctick %u.%06u %u", dir, t.tv_sec, t.tv_usec,x);
+    DoPlot(pl,"%ctick %s %u", dir, xp_timestamp(t), x);
 }
 
 
 void
 plotter_dtick(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_tick(pl,t,x,'d');
 }
@@ -310,9 +326,9 @@ plotter_dtick(pl,t,x)
 
 void
 plotter_utick(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_tick(pl,t,x,'u');
 }
@@ -320,9 +336,9 @@ plotter_utick(pl,t,x)
 
 void
 plotter_ltick(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_tick(pl,t,x,'l');
 }
@@ -330,9 +346,9 @@ plotter_ltick(pl,t,x)
 
 void
 plotter_rtick(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_tick(pl,t,x,'r');
 }
@@ -340,9 +356,9 @@ plotter_rtick(pl,t,x)
 
 void
 plotter_htick(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_tick(pl,t,x,'h');
 }
@@ -350,9 +366,9 @@ plotter_htick(pl,t,x)
 
 void
 plotter_vtick(pl,t,x)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
 {
     plotter_tick(pl,t,x,'v');
 }
@@ -361,14 +377,11 @@ plotter_vtick(pl,t,x)
 
 void
 plotter_text(pl,t,x,where,str)
-     PLOTTER pl;
-     struct timeval	t;
-     unsigned long	x;
-     char		*where;
-     char		*str;
+    PLOTTER pl;
+    struct timeval	t;
+    unsigned long	x;
+    char		*where;
+    char		*str;
 {
-	DoPlot(pl,"%stext %u.%06u %u\n%s",
-	     where,
-	     t.tv_sec, t.tv_usec,x,
-	     str);
+    DoPlot(pl,"%stext %s %u\n%s", where, xp_timestamp(t), x, str);
 }
