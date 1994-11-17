d048d1a56e8c41b4b9f98fccbaa126aad602c9c7
sdo <>
1994-11-17 21:17:39 +0000

    changed to match my coding standard

--- rexmit.c
+++ rexmit.c
@@ -25,47 +25,61 @@ It can also keep track of packets that come out of order.
 
 /* local routine definitions*/
 static int boundary(u_long,u_long);
-static int notoverlap(seqspace *,quadrant *,u_long,u_long);
-static int overlap(seqspace *,quadrant *,u_long,u_long);
-static seg_acked * create_seg(u_long,u_long);
-static quadrant * create_quadrant();
-static quadrant * whichquad(seqspace *,u_long);
-static quadrant * return_quad(seqspace *,quadrant *,int);
+static int notoverlap(seqspace *,quadrant *,u_long,u_long,u_int *);
+static int overlap(seqspace *,quadrant *,u_long,u_long,u_int *);
+static seg_acked *create_seg(u_long,u_long);
+static quadrant *create_quadrant();
+static quadrant *whichquad(seqspace *,u_long);
+static quadrant *return_quad(seqspace *,quadrant *,int);
 static void collapse(seqspace *,u_long);
 
 
 
+/*
+ * rexmit: is the specified segment a retransmit?
+ *   returns: number of retransmitted bytes in segment, 0 if not a rexmit
+ *            *pooo to to TRUE if segment is out of order
+ */
 int rexmit(
-    seqspace * sspace,
+    seqspace *sspace,
     u_long seqno,
-    u_long len)
+    u_long len,
+    u_int  *pooo) /* out of order */
 {
     quadrant *wquad;
 
-    if ((sspace->begin == 0)&&(sspace->end==0))
-    {
+    /* unless told otherwise, it's IN order */
+    *pooo = 0;
+
+    if ((sspace->begin == 0)&&(sspace->end==0)) {
 	sspace->begin = seqno;
 	sspace->end = seqno + len-1;
     }
+
+    /* see which quadrant it starts in */
     wquad = whichquad(sspace,seqno);
-    if (wquad->f_ack == NULL)
-    {
-	wquad->f_ack = (seg_acked *)create_seg(seqno,len-1);
+
+    /* if first segment in quadrant, there's nothing else to do */
+    if (wquad->f_ack == NULL) {
+	/* Hmmm... what if the first segments wraps into the next quadrant? */
+	wquad->f_ack = create_seg(seqno,len-1);
 	wquad->l_ack = wquad->f_ack;
 	return(0);
-    }	
-    collapse(sspace,seqno);
-    if (boundary(seqno,seqno+len-1)) 
-    {
-	return(overlap(sspace,wquad,seqno,len-1));
     }
-    else
-	return(notoverlap(sspace,wquad,seqno,len-1));
 
+    /* collapse the sequence space so far */
+    collapse(sspace,seqno);
+
+    /* add the new segment into the segment database */
+    if (boundary(seqno,seqno+len-1)) 
+	return(overlap(sspace,wquad,seqno,len-1,pooo));
+    else
+	return(notoverlap(sspace,wquad,seqno,len-1,pooo));
 }
 
 
 /*******************************************************************/
+/* boundary: does the segment cross a quadrant boundary? */
 int boundary(
     u_long beg,
     u_long fin)
@@ -87,78 +101,65 @@ notoverlap(
     seqspace *sspace,
     quadrant *wquad,
     u_long seqno,
-    u_long len)
+    u_long len,
+    u_int  *pooo) /* out of order */
 {
-    int outoforder=0;
     seg_acked *q,*tmp;
     int last_sent =0,rexlen=0;
 
+    *pooo = 0;
     q = wquad->f_ack;
-    while (q != NULL) 
-    {
-	if ((q->beg<= seqno)&&(seqno<= q->sent_end))
-	{
+    while (q != NULL) {
+	if ((q->beg<= seqno)&&(seqno<= q->sent_end)) {
 	    if (seqno+len<=q->sent_end)
 		return(len+1);
-	    else 
-	    {
-		tmp = q->next;
-		while (tmp!=NULL)
-		{
-		    if ((seqno+len>=tmp->beg)&&(seqno+len<=tmp->sent_end))
-			return(len+1);
-		    else if (seqno+len < tmp->beg)
-		    {
-			last_sent = tmp->prev->sent_end;
-			break;
-		    }
-		    tmp = tmp->next;
+
+	    tmp = q->next;
+	    while (tmp!=NULL) {
+		if ((seqno+len>=tmp->beg)&&(seqno+len<=tmp->sent_end))
+		    return(len+1);
+		else if (seqno+len < tmp->beg) {
+		    last_sent = tmp->prev->sent_end;
+		    break;
 		}
-		if (last_sent == 0 )
-		{
-		    rexlen = (wquad->l_ack->sent_end)-seqno+1;
-		    wquad->l_ack->sent_end = seqno+len;
-		    sspace->end = seqno+len;
-		    return(rexlen);
-		}
-		else 
-		{
-		    rexlen = last_sent+1-seqno; 
-		    tmp->prev->sent_end = seqno+len;
-		    return(rexlen);
-		}
+		tmp = tmp->next;
 	    }
-	}
-	else if ((q->beg<=seqno)&&(seqno>q->sent_end)&&(seqno==(q->sent_end+1))&&(q->next==NULL))
-	{
+	    if (last_sent == 0 ) {
+		rexlen = (wquad->l_ack->sent_end)-seqno+1;
+		wquad->l_ack->sent_end = seqno+len;
+		sspace->end = seqno+len;
+		return(rexlen);
+	    } else {
+		rexlen = last_sent+1-seqno; 
+		tmp->prev->sent_end = seqno+len;
+		return(rexlen);
+	    }
+	} else if ((q->beg<=seqno)&&(seqno>q->sent_end)&&
+		   (seqno==(q->sent_end+1))&&(q->next==NULL)) {
 	    q->sent_end = q->sent_end +1+ len;	
 	    sspace->end = q->sent_end;
 	    return(0);
-	}
-	else if ((q->next==NULL)&&((seqno>q->sent_end+1)||
-				   ((seqno<q->beg)&&(seqno+len<q->beg))))
-	{
+	} else if ((q->next==NULL)&&((seqno>q->sent_end+1)||
+				     ((seqno<q->beg)&&(seqno+len<q->beg)))) {
 	    if (seqno<q->beg) 
-		outoforder = 1;
+		*pooo = 1;
 	    tmp = (seg_acked *)create_seg(seqno,len);
-	    if (outoforder)
-	    {
+	    if (*pooo) {
 		tmp->prev = q->prev;
-		q->prev->next = tmp;
+		if (q->prev)
+		    q->prev->next = tmp;
+		else
+		    wquad->f_ack = tmp;  /* new head of list */
 		q->prev = tmp;
 		tmp->next = q;
-		
-	    }
-	    else
-	    {
+	    } else {
 		q->next = tmp;
 		tmp->prev = q;
 		sspace->end = tmp->sent_end;
 	 	wquad->l_ack = tmp;
 	    }
 	    return(0);
-	}
-	else
+	} else
 	    q = q->next;
     }
     /* should never happen */
@@ -170,20 +171,18 @@ int overlap(
     seqspace *sspace,
     quadrant *qu,
     u_long seq,
-    u_long len)
+    u_long len,
+    u_int  *pooo) /* out of order */
 {
     seg_acked *q,*r,*tmp,*temp;
     int partial=0,rexlen=0;
-    int outoforder = 0;
 
+    *pooo = 0;
     q=qu->f_ack;
-    while (q!=NULL)
-    {
-	if ((seq >= q->beg)&&(seq<=q->sent_end))
-	{
+    while (q!=NULL) {
+	if ((seq >= q->beg)&&(seq<=q->sent_end)) {
 	    r= qu->next->f_ack;
-	    while (r!=NULL)
-	    {
+	    while (r!=NULL) {
 		if ((seq+len>=r->beg)&&(seq+len<=r->sent_end))
 		    return(len+1);
 		else
@@ -194,12 +193,11 @@ int overlap(
 	if (!partial)
 	    q = q->next;
     }
-    if (partial)
-    {
+
+    if (partial) {
 	rexlen= q->sent_end - seq+1;
 	tmp = q->next;
-	while (tmp!=NULL)
-	{
+	while (tmp!=NULL) {
 	    rexlen = rexlen + tmp->sent_end +1 - tmp->beg;
 	    tmp = tmp->next;
 	}
@@ -207,38 +205,35 @@ int overlap(
 	qu->l_ack->sent_end = qu->quad_end;
 	q->prev->next = qu->l_ack;
 	qu->l_ack->prev = q->prev;
-	while (q!=qu->l_ack)
-	{
+	while (q!=qu->l_ack) {
 	    temp = q->next;
 	    free(q);
 	    q=temp;
 	}
-    }
-    else
-    {
+    } else {
 	qu->l_ack->next = create_seg(seq,qu->quad_end-seq);
 	qu->l_ack->next->prev = qu->l_ack;
 	qu->l_ack = qu->l_ack->next;
 	qu->l_ack->next = NULL;
     }
-    if ((qu->next->f_ack == NULL)&&(partial))
-    {
+
+
+    if ((qu->next->f_ack == NULL)&&(partial)) {
 	qu->next->f_ack = create_seg(qu->quad_end+1,(seq+len)-(qu->quad_end+1));
 	qu->next->l_ack = qu->next->f_ack;
 	qu->next->full = 0;
 	sspace->end = seq+len;
 	return(rexlen);
-    }
-    else if ((qu->next->f_ack!=NULL)&&(seq+len<qu->next->f_ack->beg)
-	     &&(!partial))	
-    {
-	outoforder = 1;
+    } else if ((qu->next->f_ack!=NULL)&&(seq+len<qu->next->f_ack->beg)
+	       &&(!partial)) {
+	*pooo = 1;
 	tmp = create_seg(qu->quad_end+1,(seq+len)-(qu->quad_end+1));
 	tmp->next = qu->next->f_ack;
 	qu->next->f_ack->prev = tmp;
 	qu->next->f_ack = tmp;
 	return(0);
     }
+
     /* should never happen */
     return(0);
 }
@@ -257,7 +252,8 @@ create_seg(
     ptr->beg = seq;
     ptr->sent_end = seq+len;
     ptr->prev = NULL;
-    ptr->next=NULL;
+    ptr->next = NULL;
+
     return(ptr);
 }
 
@@ -269,6 +265,7 @@ create_quadrant()
 
     ptr = (quadrant *)malloc(sizeof(quadrant));
     bzero(ptr,sizeof(quadrant));
+
     return(ptr);
 }
 
@@ -301,16 +298,14 @@ return_quad(
     quadrant *ptr,
     int id)
 {
-    if (ptr == NULL)
-    {	
+    if (ptr == NULL) {	
 	ptr = create_quadrant();
 	ptr->full = 0;
     }
-    if (id==1)
-    {
+
+    if (id==1) {
 	sspace->q1 = ptr;
-	if ((sspace->q2 == NULL)||(sspace->q4==NULL))
-	{
+	if ((sspace->q2 == NULL)||(sspace->q4==NULL)) {
 	    if (sspace->q2 == NULL)
 		sspace->q2=create_quadrant();
 	    if (sspace->q4 == NULL)
@@ -325,12 +320,9 @@ return_quad(
 	    if (sspace->q3 != NULL)
 		free((quadrant *)sspace->q3);
 	}
-    }
-    else if (id==2)
-    {
+    } else if (id==2) {
 	sspace->q2 = ptr;
-	if  ((sspace->q3==NULL)||(sspace->q1==NULL))
-	{
+	if  ((sspace->q3==NULL)||(sspace->q1==NULL)) {
 	    if (sspace->q3 == NULL)
 		sspace->q3=create_quadrant();
 	    if (sspace->q1 == NULL)
@@ -344,9 +336,7 @@ return_quad(
 	    if (sspace->q4 != NULL)
 		free((quadrant *)sspace->q4);
 	}
-    }
-    else if (id==3)
-    {
+    } else if (id==3) {
 	sspace->q3 = ptr;
 	if  ((sspace->q4==NULL)||(sspace->q2==NULL))
 	{
@@ -363,12 +353,9 @@ return_quad(
 	    if (sspace->q1 != NULL)
 		free((quadrant *)sspace->q1);
 	}
-    }
-    else if (id==4)
-    {
+    } else if (id==4) {
 	sspace->q4 = ptr;
-	if  ((sspace->q1==NULL)||(sspace->q3==NULL))
-	{
+	if  ((sspace->q1==NULL)||(sspace->q3==NULL)) {
 	    if (sspace->q1 == NULL)
 		sspace->q1=create_quadrant();
 	    if (sspace->q3 == NULL)
@@ -397,16 +384,12 @@ void collapse(
 
 
     present = whichquad(sspace,seq);
-    while(index<=4)
-    {
-	if (present != NULL)
-	{
+    while(index<=4) {
+	if (present != NULL) {
 	    seg = present->f_ack;
-	    while (seg !=NULL)
-	    {
+	    while (seg !=NULL) {
 		freed = 0;
-		if ((seg->next!=NULL)&&(seg->sent_end+1 == seg->next->beg))
-		{
+		if ((seg->next!=NULL)&&(seg->sent_end+1 == seg->next->beg)) {
 		    seg->sent_end = seg->next->sent_end;
 		    tmp = seg->next;
 		    seg->next = seg->next->next;
@@ -420,12 +403,11 @@ void collapse(
 		if (!freed)
 		    seg = seg->next;
 	    }
-	    if (present->f_ack!=NULL)
-	    {
-		if ((present->f_ack->sent_end ==0x3fffffff)||
-		    (present->f_ack->sent_end==0x7fffffff)||
+	    if (present->f_ack!=NULL) {
+		if ((present->f_ack->sent_end == 0x3fffffff)||
+		    (present->f_ack->sent_end == 0x7fffffff)||
 		    (present->f_ack->sent_end == 0xbfffffff)||
-		    (present->f_ack->sent_end==0xffffffff))
+		    (present->f_ack->sent_end == 0xffffffff))
 		    present->full = 1;
 	    }
 	}
