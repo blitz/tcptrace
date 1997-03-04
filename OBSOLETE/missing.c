3f03777b1feb086c4e04e0f882b796eeafc28515
sdo <>
1997-03-04 18:03:03 +0000

    latest

---
+++ OBSOLETE/missing.c
@@ -0,0 +1,27 @@
+/* routines that were missing somewhere.  Not really part of the
+   distribution, but an attempt to help out people that want to port
+   this thing  */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+
+#ifdef NEED_ETHER_NTOA
+/* ether_ntoa doesn't exist on at least some HP machines. */
+/* how about: */
+
+char *
+ether_ntoa (struct ether_addr *e)
+{
+    unsigned char *pe;
+    static char buf[30];
+
+    pe = (unsigned char *) e;
+    sprintf(buf,"%02x:%02x:%02x:%02x:%02x:%02x",
+	    pe[0], pe[1], pe[2], pe[3], pe[4], pe[5]);
+    return(buf);
+}
+#endif NEED_ETHER_NTOA
