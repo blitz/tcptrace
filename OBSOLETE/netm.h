49dbdf5d3fb262a6e799f544f74fe562c535ec0f
sdo <>
1997-03-05 07:23:23 +0000

    latest

--- OBSOLETE/netm.h
+++ OBSOLETE/netm.h
@@ -34,33 +34,32 @@ static char const rcsid[] =
 #define NETM_DUMP_OFFSET 0x1000
 
 
-
 /* netm file header format */
 struct netm_header {
 	int	netm_key;
 	int	version;
 };
-#define VERSION_OLD 3
-#define VERSION_NEW 4
+#define NETM_VERSION_OLD 3
+#define NETM_VERSION_NEW 4
 #define NETM_KEY 0x6476
 
 
 /* netm packet header format */
-struct packet_header_old {
-	int	unused1;
-	int	unused2;
-	int	tstamp_secs;
-	int	tstamp_usecs;
-	int	unused3;
-	int	len;
+struct netm_packet_header_old {
+    int	unused1;
+    int	unused2;
+    int	tstamp_secs;
+    int	tstamp_usecs;
+    int	tlen;
+    int	len;
 };
-struct packet_header {
-	int	unused1;
-	int	tstamp_secs;
-	int	tstamp_usecs;
-	int	unused2;
-	int	unused3;
-	int	len;
-	int	tlen;  /* truncated length */
-	int	unused5;
+struct netm_packet_header {
+    int	unused1;
+    int	tstamp_secs;
+    int	tstamp_usecs;
+    int	unused2;
+    int	unused3;
+    int	len;
+    int	tlen;  /* truncated length */
+    int	unused5;
 };
