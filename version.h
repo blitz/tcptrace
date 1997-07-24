3729900709d3966f13325af68c3d8a7d0da446d1
sdo <>
1997-07-24 19:41:04 +0000

    latest

--- version.h
+++ version.h
@@ -29,4 +29,4 @@ static char const rcsid_version[] =
     "@(#)$Header$";
 
 
-#define VERSION "Ostermann's tcptrace -- version 4.0.2 -- Wed Jul 16, 1997"
+#define VERSION "Ostermann's tcptrace -- version 4.0.3 -- Thu Jul 24, 1997"
