c8f9db693ea4b1e5f7de6d3e5b0584b674f75453
sdo <>
1997-11-12 15:01:32 +0000

    added interfaces for new module-specific data structures and   new module calls

--- mod_http.h
+++ mod_http.h
@@ -1,5 +1,7 @@
 /* header file for http.c */
 int http_init(int argc, char *argv[]);
-void http_read(struct ip *pip, tcp_pair *ptp, void *plast);
+void http_read(struct ip *pip, tcp_pair *ptp, void *plast, void *pmod_data);
 void http_done(void);
 void http_usage(void);
+void http_newfile(char *newfile);
+void *http_newconn(tcp_pair *ptp);
