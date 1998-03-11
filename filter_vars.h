fb532e821ae3fa75ed1034db2c76f3bb76b3eac7
sdo <>
1998-03-11 05:45:10 +0000

    adeed a bunch of new fields

--- filter_vars.h
+++ filter_vars.h
@@ -30,41 +30,68 @@
 /* just a big table of things that we can filter on */
 static tcp_pair ptp_dummy;
 #define PTCB_C_S(field) &ptp_dummy.a2b.field,&ptp_dummy.b2a.field
+#define PTP(a,b)	&ptp_dummy.a,&ptp_dummy.b
+#define PTPA(a,b)	&ptp_dummy.addr_pair.a,&ptp_dummy.addr_pair.b
 struct filter_line filters[] = {
-    {"hostname",	V_STRING,&ptp_dummy.a_hostname,&ptp_dummy.b_hostname},
-    {"portname",	V_STRING,&ptp_dummy.a_portname,&ptp_dummy.b_portname},
-    {"port",		V_USHORT,&ptp_dummy.addr_pair.a_port,&ptp_dummy.addr_pair.b_port},
+    {"hostname",	V_STRING, PTP(a_hostname,b_hostname),"FQDN host name (unless -n)"},
+    {"portname",	V_STRING, PTP(a_portname,b_portname),"service name of the port (unless -n)"},
+    {"port",		V_USHORT, PTPA(a_port,b_port),"port NUMBER"},
 
-    {"f1323_ws",	V_BOOL,	PTCB_C_S(f1323_ws)},
-    {"f1323_ts",	V_BOOL,	PTCB_C_S(f1323_ts)},
-    {"fsack_req",	V_BOOL,	PTCB_C_S(fsack_req)},
-    {"window_scale",	V_BOOL,	PTCB_C_S(window_scale)},
+    {"mss",		V_INT,	PTCB_C_S(mss),"maximum segment size"},
+    {"f1323_ws",	V_BOOL,	PTCB_C_S(f1323_ws),"1323 window scaling requested"},
+    {"f1323_ts",	V_BOOL,	PTCB_C_S(f1323_ts),"1323 time stampts requested"},
+    {"fsack_req",	V_BOOL,	PTCB_C_S(fsack_req),"SACKs requested"},
+    {"window_scale",	V_BOOL,	PTCB_C_S(window_scale),"window scale factor"},
 
-    {"data_bytes",	V_ULLONG,	PTCB_C_S(data_bytes)},
-    {"data_pkts",	V_ULLONG,	PTCB_C_S(data_pkts)},
-    {"data_pkts_push",	V_ULLONG,	PTCB_C_S(data_pkts_push)},
-    {"rexmit_bytes",	V_ULLONG,	PTCB_C_S(rexmit_bytes)},
-    {"rexmit_pkts",	V_ULLONG,	PTCB_C_S(rexmit_pkts)},
-    {"ack_pkts",	V_ULLONG,	PTCB_C_S(ack_pkts)},
-    {"win_max",		V_ULONG,	PTCB_C_S(win_max)},
-    {"win_min",		V_ULONG,	PTCB_C_S(win_min)},
-    {"win_tot",		V_ULONG,	PTCB_C_S(win_tot)},
-    {"win_zero_ct",	V_ULONG,	PTCB_C_S(win_zero_ct)},
-    {"min_seq",		V_ULONG,	PTCB_C_S(min_seq)},
-    {"max_seq",		V_ULONG,	PTCB_C_S(max_seq)},
-    {"packets",		V_ULLONG,	PTCB_C_S(packets)},
-    {"syn_count",	V_UCHAR,	PTCB_C_S(syn_count)},
-    {"fin_count",	V_UCHAR,	PTCB_C_S(fin_count)},
-    {"reset_count",	V_UCHAR,	PTCB_C_S(reset_count)},
-    {"min_seg_size",	V_ULONG,	PTCB_C_S(min_seg_size)},
-    {"max_seg_size",	V_ULONG,	PTCB_C_S(max_seg_size)},
-    {"out_order_pkts",	V_ULLONG,	PTCB_C_S(out_order_pkts)},
-    {"sacks_sent",	V_ULLONG,	PTCB_C_S(sacks_sent)},
-    {"ipv6_segments",	V_ULONG,	PTCB_C_S(ipv6_segments)},
+    {"data_bytes",	V_ULLONG, PTCB_C_S(data_bytes),"bytes of data"},
+    {"data_segs",	V_ULLONG, PTCB_C_S(data_pkts),"segments of data"},
+    {"data_segs_push",	V_ULLONG, PTCB_C_S(data_pkts_push),"segments with PUSH set"},
+    {"rexmit_bytes",	V_ULLONG, PTCB_C_S(rexmit_bytes),"retransmitted bytes"},
+    {"rexmit_segs",	V_ULLONG, PTCB_C_S(rexmit_pkts),"segments w/ retransmitted data"},
+    {"ack_segs",	V_ULLONG, PTCB_C_S(ack_pkts),"segments containing ACK"},
+    {"win_max",		V_ULONG,  PTCB_C_S(win_max),"MAX window advertisement"},
+    {"win_min",		V_ULONG,  PTCB_C_S(win_min),"MIN window advertisement"},
+    {"win_zero_ct",	V_ULONG,  PTCB_C_S(win_zero_ct),"number of ZERO windows advertised"},
+    {"min_seq",		V_ULONG,  PTCB_C_S(min_seq),"smallest sequence number"},
+    {"max_seq",		V_ULONG,  PTCB_C_S(max_seq),"largest sequence number"},
+    {"packets",		V_ULLONG, PTCB_C_S(packets),"total segments"},
+    {"syn_count",	V_UCHAR,  PTCB_C_S(syn_count),"SYNs sent"},
+    {"fin_count",	V_UCHAR,  PTCB_C_S(fin_count),"FINs sent"},
+    {"reset_count",	V_UCHAR,  PTCB_C_S(reset_count),"RESETs sent"},
+    {"min_seg_size",	V_ULONG,  PTCB_C_S(min_seg_size),"smallest amount of data in a segment (not 0)"},
+    {"max_seg_size",	V_ULONG,  PTCB_C_S(max_seg_size),"largest amount of data in a segment"},
+    {"out_order_segs",	V_ULLONG, PTCB_C_S(out_order_pkts),"out of order segments"},
+    {"sacks_sent",	V_ULLONG, PTCB_C_S(sacks_sent),"SACKs sent"},
+    {"ipv6_segs",	V_ULONG,  PTCB_C_S(ipv6_segments),"number of IPv6 segments sent"},
+
+    {"num_hw_dups",     V_ULONG,  PTCB_C_S(num_hardware_dups),"number of hardware-level duplicates"},
+
+    {"initwin_bytes",   V_ULONG,  PTCB_C_S(initialwin_bytes),"number of bytes in initial window"},
+    {"initwin_segs",    V_ULONG,  PTCB_C_S(initialwin_segs),"number of segments in initial window"},
+
+    {"rtt_min",         V_ULONG, PTCB_C_S(rtt_min), "MIN round trip time (usecs)"},
+    {"rtt_max",         V_ULONG, PTCB_C_S(rtt_max), "MAX round trip time (usecs)"},
+    {"rtt_count",       V_ULONG, PTCB_C_S(rtt_count), "number of RTT samples"},
+
+    {"rtt_min_last",    V_ULONG, PTCB_C_S(rtt_min_last), "MIN round trip time (usecs) (from last rexmit)"},
+    {"rtt_max_last",    V_ULONG, PTCB_C_S(rtt_max_last), "MAX round trip time (usecs) (from last rexmit)"},
+    {"rtt_count_last",  V_ULONG, PTCB_C_S(rtt_count_last), "number of RTT samples (from last rexmit)"},
+
+    {"rtt_amback",      V_ULLONG, PTCB_C_S(rtt_amback), "number of ambiguous ACKs"},
+    {"rtt_cumack",      V_ULLONG, PTCB_C_S(rtt_cumack), "number of cumulative ACKs"},
+    {"rtt_unkack",      V_ULLONG, PTCB_C_S(rtt_unkack), "number of unknown ACKs"},
+    {"rtt_dupack  ",    V_ULLONG, PTCB_C_S(rtt_dupack), "number of duplicate ACKs"},
+
+    {"retr_max",        V_ULONG, PTCB_C_S(retr_max), "MAX rexmits of a single segment"},
+    {"retr_min_tm",     V_ULONG, PTCB_C_S(retr_min_tm), "MIN time until rexmit (usecs)"},
+    {"retr_max_tm",     V_ULONG, PTCB_C_S(retr_max_tm), "MAX time until rexmit (usecs)"},
+
+    {"extr_trunc_bytes",V_ULLONG, PTCB_C_S(extr_trunc_bytes), "number of bytes not in the file"},
+    {"extr_trunc_segs", V_ULLONG, PTCB_C_S(extr_trunc_segs), "number of segments not in the file"},
 
     /* computed functions */
 
     /* throughput in bytes/second - 0 for infinite or none */
-    {"thruput",		V_UFUNC,	&VFuncClntTput, &VFuncServTput},
+    {"thruput",		V_UFUNC, &VFuncClntTput, &VFuncServTput, "thruput (bytes/sec)"},
 };
 #define NUM_FILTERS (sizeof(filters)/sizeof(struct filter_line))
