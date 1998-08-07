1db82c5e4d629614a300c702b38f9e6fd5275226
sdo <>
1998-08-07 21:06:59 +0000

    latest

--- version.h
+++ version.h
@@ -30,7 +30,7 @@ static char const rcsid_version[] =
 
 
 /* source code version information */
-#define VERSION "Ostermann's tcptrace -- version 5.0.5 -- Thu Jul 23, 1998"
+#define VERSION "Ostermann's tcptrace -- version 5.0.5e -- Fri Aug  7, 1998"
 
 /* build information */
 /* constants filled in when version.c is compiled */
