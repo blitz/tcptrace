73be08cb3f93cafa99552a33fba559364a9eb6c2
sdo <>
1999-02-17 20:15:50 +0000

    latest

--- version.h
+++ version.h
@@ -32,8 +32,8 @@ static char const rcsid_version[] =
 /* source code version information */
 #define VERSION_MAJOR	5
 #define VERSION_MINOR	1
-#define VERSION_BUGFIX	4
-#define VERSION_DATE	"Wed Jan 27, 1999"
+#define VERSION_BUGFIX	5
+#define VERSION_DATE	"Wed Feb 17, 1999"
 
 
 
