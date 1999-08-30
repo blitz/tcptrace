79549ee8cb1ee3e0ed6110e84f971e47491e26e6
sdo <>
1999-08-30 14:32:12 +0000

    latest version

--- dyncounter.h
+++ dyncounter.h
@@ -2,14 +2,24 @@
 typedef struct dyn_counter *dyn_counter;
 
 
-/* access routines */
-void DestroyCounters(dyn_counter *phandle);
-void AddToCounter(dyn_counter *phandle, unsigned long ix, unsigned long val);
-void SetCounter(dyn_counter *phandle, unsigned long ix, unsigned long val);
-unsigned long MaxCounter(dyn_counter handle);
-unsigned long MinCounter(dyn_counter handle);
-unsigned long TotalCounter(dyn_counter handle);
+/* adding/setting counters */
+void AddToCounter(dyn_counter *phandle, unsigned long ix,
+		  unsigned long val, unsigned long granularity);
+void SetCounter(dyn_counter *phandle, unsigned long ix,
+		unsigned long val, unsigned long granularity);
+
+
+/* lookup various counter values */
+unsigned long GetMaxIx(dyn_counter handle);
+unsigned long GetMinIx(dyn_counter handle);
+unsigned long GetMaxCount(dyn_counter handle);
+unsigned long GetTotalCounter(dyn_counter handle);
+unsigned long GetGran(dyn_counter handle);
+
+/* query counter values */
 unsigned long LookupCounter(dyn_counter handle, unsigned long ix);
 int NextCounter(dyn_counter *phandle, void *pcookie,
 		unsigned long *pix, unsigned long *pcount);
 
+/* when you're finished */
+void DestroyCounters(dyn_counter *phandle);
