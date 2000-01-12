8080419d0494e3380100c5bb225389cfd4faadda
sdo <>
2000-01-12 18:54:13 +0000

    initial

---
+++ mod_slice.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 1994, 1995, 1996, 1997, 1998, 1999, 2000
+ *	Ohio University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that: (1) source code
+ * distributions retain the above copyright notice and this paragraph
+ * in its entirety, (2) distributions including binary code include
+ * the above copyright notice and this paragraph in its entirety in
+ * the documentation or other materials provided with the
+ * distribution, and (3) all advertising materials mentioning features
+ * or use of this software display the following acknowledgment:
+ * ``This product includes software developed by the Ohio University
+ * Internetworking Research Laboratory.''  Neither the name of the
+ * University nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific
+ * prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * Author:	Shawn Ostermann
+ * 		School of Electrical Engineering and Computer Science
+ * 		Ohio University
+ * 		Athens, OH
+ *		ostermann@cs.ohiou.edu
+ */
+static char const rcsid_slice[] =
+    "@(#)$Header$";
+
+
+/* header file for slice.c */
+int slice_init(int argc, char *argv[]);
+void slice_read(struct ip *pip, tcp_pair *ptp, void *plast, void *pmod_data);
+void slice_done(void);
+void slice_usage(void);
+void *slice_newconn(tcp_pair *ptp);
