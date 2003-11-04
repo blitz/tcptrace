b5c2394d8c2ba3b77351607610a49d964b96734f
mramadas <>
2003-11-04 19:44:46 +0000

    Updating CHANGES and version.h for release 6.6.0

--- version.h
+++ version.h
@@ -57,9 +57,9 @@ static char const rcsid_version[] =
 
 /* source code version information */
 #define VERSION_MAJOR	"6"
-#define VERSION_MINOR	"4"
-#define VERSION_BUGFIX	"16"
-#define VERSION_DATE	"Fri Oct 31, 2003"
+#define VERSION_MINOR	"6"
+#define VERSION_BUGFIX	"0"
+#define VERSION_DATE	"Tue Nov 4, 2003"
 
 #define VERSION_NUM   VERSION_MAJOR "." VERSION_MINOR "." VERSION_BUGFIX
 
